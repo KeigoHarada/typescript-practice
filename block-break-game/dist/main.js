f3160581c3e8041052d72ff6cb1210aa4a7dacee
KeigoHarada <hkddna5@gmail.com>
2024-09-16 23:54:00 +0900

    canvasに書ける様になった

---
+++ block-break-game/dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+if (!ctx) {
+    throw new Error('Canvas context not supported');
+}
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = 'green';
+ctx.fill();
+ctx.closePath();
